04250c1b346a57c602a8583d2422f471bc97de52
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-02 15:22:08 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,7 +6,7 @@ SCENARIO ("array")
   int a[] = {3, 2, 4, 5, 6};
   int* s=a;
   int* l=a+5;
- sort_iterator(s,1);
+ insertion_sort(s,1);
  REQUIRE(a[0] == 2);
  REQUIRE(a[1] == 3);
  REQUIRE(a[2] == 4);
@@ -18,7 +18,7 @@ SCENARIO("matr")
   int arr[3][3] = {4, 21, 12, 5, 2, 7, 42, 26, 8};
   int * beg = &arr[0][0];
   int * end = &arr[2][3];
-  sort_iterator(beg,end);
+  insertion_sort(beg,end);
   REQUIRE(arr[0][0] == 2);
   REQUIRE(arr[0][1] == 4);
   REQUIRE(arr[0][2] == 5);
