9940b961242196b6404a709465e3e1464f254110
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-02 15:26:19 +0400

    Update iteratorsort.hpp

--- include/iteratorsort.hpp
+++ include/iteratorsort.hpp
@@ -5,6 +5,6 @@ void insertion_sort(iterator begin , iterator end)
    if(!(begin < end))
     return;
    for(iterator i = begin + 1 ; i != end ; ++i)
-    for(iterator j = 1; j =!begin && *j < *(j-1); --j)
+    for(iterator j = i; j =!begin && *j < *(j-1); --j)
         std::iter_swap(j - 1, j);
 }
